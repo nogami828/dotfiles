da548551a364853feb5a8872035de7ebeac57c7f
umeyuki <umeyuki1326+github@gmail.com>
2013-10-22 09:36:42 +0900

    git gutter command

--- .emacs.d/conf/04-editing.el
+++ .emacs.d/conf/04-editing.el
@@ -114,3 +114,19 @@
 (add-hook 'js2-mode-hook   'git-gutter-mode)
 (add-hook 'php-mode-hook   'git-gutter-mode)
 (add-hook 'ruby-mode-hook  'git-gutter-mode)
+
+;; If you enable git-gutter-mode for some modes
+(add-hook 'ruby-mode-hook 'git-gutter-mode)
+
+(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+
+;; Jump to next/previous hunk
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+
+;; Stage current hunk
+(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
